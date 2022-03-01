76c1fe057e89e7c6a3a7cd10699bf6350b22692c
legless <legless@legl.es>
2022-03-01 12:02:46 +0200

    Add warning to "barricades" task

--- pages/task/barricades.vue
+++ pages/task/barricades.vue
@@ -39,6 +39,8 @@ import VueMarkdown from 'vue-markdown'
 const text = `
 У координації з військовими та представниками територіальної оборони облаштовуйте завали та барикади на важливих дорогах.
 
+**Важливо:** [не блокуйте](https://dniprorada.gov.ua/uk/articles/item/49364/filatov-zaklikav-dnipryan-ne-rozstavlyati-samostijno-izhakiv-i-ogorozh-adzhe-ce-mozhe-paralizuvati-robotu-armijciv-i-proizd-shvidkih) дороги без чітких вказівок влади та/або військових, щоб не порушувати логістичних ланцюжків.
+
 Для спорудження барикад можна використовувати мішки з піском, палети для укріплення, протитанкові їжаки та інші засоби для організації барикад на вулиці, використані автомобільні шини та [інші матеріали](https://t.me/KyivCityOfficial/2165).
 
 [Джерело](https://www.facebook.com/SecurSerUkraine/posts/306813471545570#btn-center)
