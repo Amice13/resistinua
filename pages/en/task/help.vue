e9802ab95264df4858010cdd06c682ba9099e175
legless <legless@legl.es>
2022-03-02 15:16:20 +0200

    [en] Рахунок НБУ для гуманітарної допомоги.

--- pages/en/task/help.vue
+++ pages/en/task/help.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
-  <v-row justify="center" align="center" class="mb-0">
-    <v-col cols="12" sm="10" md="8" class="mb-0">
-      <v-card class="mx-auto mb-0" flat>
-        <v-card-text class="text-h4 flex-grow-1 mb-0 pb-0">
-          <div class="fill-height d-flex">
-            <v-icon class="mr-4">mdi-check-outline</v-icon> Надати гуманітарну допомогу
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="10" md="8">
-      <v-card class="mx-auto mb-4 text-justify mw-100" flat>
-        <v-card-text>
-          <vue-markdown>
-            {{ text }}
-          </vue-markdown>
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row justify="center" align="center" class="mb-0">
+      <v-col cols="12" sm="10" md="8" class="mb-0">
+        <v-card class="mx-auto mb-0" flat>
+          <v-card-text class="text-h4 flex-grow-1 mb-0 pb-0">
+            <div class="fill-height d-flex">
+              <v-icon class="mr-4">mdi-check-outline</v-icon> Provide humanitarian assistance
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="10" md="8">
+        <v-card class="mx-auto mb-4 text-justify mw-100" flat>
+          <v-card-text>
+            <vue-markdown>
+              {{ text }}
+            </vue-markdown>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -37,9 +37,13 @@
 import VueMarkdown from 'vue-markdown'
 
 const text = `
-Якщо у Вас є можливість надати гуманітарну допомогу, то звертайтеся до відповідних регіональних штабів.
+If you can provide humanitarian assistance, please, [contact](https://varta1.com.ua/news/na-lvivshchini-organizovano-shtabi-kudi-mozhna-prinesti-gumanitarnu-dopomogu_341727.html) relevant regional centers.
 
-[Надати допомогу](https://varta1.com.ua/news/na-lvivshchini-organizovano-shtabi-kudi-mozhna-prinesti-gumanitarnu-dopomogu_341727.html#btn-center)
+National Bank of Ukraine opens fundraising account for humanitarian assistance to Ukrainians affected by Russia’s aggression.
+
+This account accepts multiple currencies. It has been established and opened to receive transfers from international partners and donors both in foreign currency (U.S. dollars, euros, British pounds) and from Ukrainian business and citizens in hryvnias.
+
+[Support Ukraine](https://bank.gov.ua/en/news/all/natsionalniy-bank-vidkriv-rahunok-dlya-gumanitarnoyi-dopomogi-ukrayintsyam-postrajdalim-vid-rosiyskoyi-agresiyi#btn-center)
 
 `
 
